bf069e13ce6c7ec9fcbc76eeefb1882f8e6f252f
Danila Poyarkov <dev@dannote.net>
2026-07-26 21:44:08 +0200

    fix(migration): widen cumulative counters only

--- priv/repo/migrations/20260726000001_widen_token_counters.exs
+++ priv/repo/migrations/20260726000001_widen_token_counters.exs
@@ -10,14 +10,6 @@ defmodule LLMProxy.Repo.Migrations.WidenTokenCounters do
 
   defp widen_quackdb_columns do
     drop(index(:api_keys, [:hash]))
-    drop(index(:usage_log, [:key_id]))
-    drop(index(:usage_log, [:timestamp]))
-    drop(index(:traces, [:key_id]))
-    drop(index(:traces, [:model]))
-    drop(index(:traces, [:session_id]))
-    drop(index(:traces, [:timestamp]))
-    drop(index(:message_log, [:key_id]))
-    drop(index(:message_log, [:timestamp]))
 
     alter table(:api_keys) do
       modify(:quota_4h_input, :bigint)
@@ -51,46 +43,6 @@ defmodule LLMProxy.Repo.Migrations.WidenTokenCounters do
       modify(:cache_write_tokens, :bigint)
     end
 
-    alter table(:usage_log) do
-      modify(:input_tokens, :bigint)
-    end
-
-    alter table(:usage_log) do
-      modify(:output_tokens, :bigint)
-    end
-
-    alter table(:usage_log) do
-      modify(:cache_read_tokens, :bigint)
-    end
-
-    alter table(:usage_log) do
-      modify(:cache_write_tokens, :bigint)
-    end
-
-    alter table(:traces) do
-      modify(:input_tokens, :bigint)
-    end
-
-    alter table(:traces) do
-      modify(:output_tokens, :bigint)
-    end
-
-    alter table(:message_log) do
-      modify(:input_tokens, :bigint)
-    end
-
-    alter table(:message_log) do
-      modify(:output_tokens, :bigint)
-    end
-
     create(unique_index(:api_keys, [:hash]))
-    create(index(:usage_log, [:key_id]))
-    create(index(:usage_log, [:timestamp]))
-    create(index(:traces, [:key_id]))
-    create(index(:traces, [:model]))
-    create(index(:traces, [:session_id]))
-    create(index(:traces, [:timestamp]))
-    create(index(:message_log, [:key_id]))
-    create(index(:message_log, [:timestamp]))
   end
 end
